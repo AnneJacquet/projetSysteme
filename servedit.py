4edaf5b9c5fa3da548f486dac8280613d7aed7e1
Thomas SCHERSACH <tirke@mbp-de-thomas.home>
2016-12-20 15:15:10 +0100

    Adapting to drive requierements

--- servedit.py
+++ servedit.py
@@ -4,23 +4,27 @@ import sys
 import socket
 import select
 
-HOST = ''
+HOST = 'localhost'
 SOCKET_LIST = []
 RECV_BUFFER = 4096
 PORT = 9000
 
 
-def chat_server():
+def drive_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((HOST, PORT))
+    try:
+        server_socket.bind((HOST, PORT))
+    except socket.error:
+        print "Socket binding failed"
+        sys.exit()
+
     server_socket.listen(10)
 
     # add server socket object to the list of readable connections
     SOCKET_LIST.append(server_socket)
 
-    print "Chat server started on port " + str(PORT)
-    print "Hostname" + str(HOST)
+    print "Chat server started on " + str(HOST) + ":" + str(PORT)
 
     while 1:
 
@@ -29,14 +33,14 @@ def chat_server():
         ready_to_read, ready_to_write, in_error = select.select(SOCKET_LIST, [], [], 0)
 
         for sock in ready_to_read:
-            # a new connection request recieved
+            # a new connection request received
             print "ready to read"
             if sock == server_socket:
                 sockfd, addr = server_socket.accept()
                 SOCKET_LIST.append(sockfd)
                 print "Client (%s, %s) connected" % addr
 
-                broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room\n" % addr)
+                # broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room\n" % addr)
 
             # a message from a client, not a new connection
             else:
@@ -46,37 +50,37 @@ def chat_server():
                     data = sock.recv(RECV_BUFFER)
                     if data:
                         # there is something in the socket
-                        broadcast(server_socket, sock, "\r" + '=>[' + str(sock.getpeername()) + '] ' + data)
-                    else:
-                        # remove the socket that's broken
+                        # broadcast(server_socket, sock, "\r" + '=>[' + str(sock.getpeername()) + '] ' + data)
+                        print "Broadcast here"
+                    else:  # remove the socket that's broken
                         if sock in SOCKET_LIST:
                             SOCKET_LIST.remove(sock)
 
-                        # at this stage, no data means probably the connection has been broken
-                        broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+                            # at this stage, no data means probably the connection has been broken
+                            # broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
 
-                        # exception
-                except:
-                    broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+                            # exception
+                except socket.error:
+                    # broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
                     continue
 
-    server_socket.close()
+    server_socket.close()  # broadcast chat messages to all connected clients
 
 
-# broadcast chat messages to all connected clients
-def broadcast(server_socket, sock, message):
-    for socket in SOCKET_LIST:
-        # send the message only to peer
-        if socket != server_socket and socket != sock:
-            try:
-                socket.send(message)
-            except:
-                # broken socket connection
-                socket.close()
-                # broken socket, remove it
-                if socket in SOCKET_LIST:
-                    SOCKET_LIST.remove(socket)
+
+# def broadcast(server_socket, sock, message):
+#     for socket in SOCKET_LIST:
+#         # send the message only to peer
+#         if socket != server_socket and socket != sock:
+#             try:
+#                 socket.send(message)
+#             except:
+#                 # broken socket connection
+#                 socket.close()
+#                 # broken socket, remove it
+#                 if socket in SOCKET_LIST:
+#                     SOCKET_LIST.remove(socket)
 
 
 if __name__ == "__main__":
-    sys.exit(chat_server())
+    sys.exit(drive_server())
